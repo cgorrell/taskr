f7898e5ec33dc5bb90a04c8f04fcbbff86b9feb0
cj <cgorrell@furbishco.com>
2014-06-16 15:49:26 -0400

    initail--heroku

---
+++ app/views.py
@@ -0,0 +1,36 @@
+from app import app, db
+from flask import flash, redirect, render_template, session, url_for
+from functools import wraps
+
+
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to log in first')
+            return redirect(url_for('users.login'))
+    return wrap
+
+
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field -%s" % (getattr(form, field).label.text, error), 'error')
+
+
+@app.route('/', defaults={'page': 'index'})
+def index(page):
+    return(redirect(url_for('users.login')))
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
+
+
+@app.errorhandler(404)
+def internal_error(error):
+    return render_template('404.html'), 404
